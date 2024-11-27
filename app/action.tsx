ed45e0c205c0d548638f6ea35a50a53d27306acd
서규원 <seokyuwon@seokyuwon-ui-MacBookAir.local>
2024-11-27 23:37:18 +0900

    zod validation 추가

--- app/action.tsx
+++ app/action.tsx
@@ -1,15 +1,52 @@
 "use server";
 
+import { z } from "zod";
+
+// const passwordRegex = new RegExp(
+//     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*?[#?!@$%^&*-]).+$/
+// );
+// 1개 이상의 숫자 포함
+const passwordRegex = new RegExp(/\d+/);
+const emailRegex = new RegExp(/@zod\.com$/);
+
+const formSchema = z.object({
+    email: z
+        .string()
+        .email()
+        .toLowerCase()
+        .regex(emailRegex, "Only @zod.com emails are allowed"),
+    username: z
+        .string({
+            invalid_type_error: "Username must be a string!",
+            required_error: "Where is my username???",
+        })
+        .min(5, "Username should be at least 5 characters long.")
+        .trim()
+        .toLowerCase(),
+    password: z
+        .string()
+        .min(10, "Password should be at least 10 characters long.")
+        .regex(
+            passwordRegex,
+            "Password should contain at least one number(0123456789)."
+        ),
+});
+
 export async function handleForm(prevState: any, formData: FormData) {
-    console.log("formData", formData);
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    if (formData.get("password") !== "12345") {
+    const data = {
+        email: formData.get("email"),
+        username: formData.get("username"),
+        password: formData.get("password"),
+    };
+    const result = formSchema.safeParse(data);
+    if (!result.success) {
         return {
-            errors: ["wrong password"],
+            success: false,
+            error: result.error.flatten(),
         };
     } else {
         return {
-            success: ["Welcome!"],
+            success: true,
         };
     }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,49 @@
 "use client";
 
-import FormButton from "@/components/form-btn";
-import FormInput from "@/components/form-input";
+import Button from "@/components/Button";
+import Input from "@/components/Input";
 import { useFormState } from "react-dom";
 import { handleForm } from "./action";
 
+const initialState = {
+    success: false,
+    error: undefined,
+};
+
 export default function Home() {
-    const [state, action] = useFormState(handleForm, null);
+    const [state, dispatch] = useFormState(handleForm, initialState);
     console.log("state", state);
     return (
         <div className="flex flex-col gap-10 py-8 px-6">
             <div className="flex flex-col gap-2 *:font-medium mt-32">
                 <h1 className="text-2xl m-auto">☃️</h1>
             </div>
-            <form action={action} className="flex flex-col gap-4">
-                <FormInput
+            <form action={dispatch} className="flex flex-col gap-4">
+                <Input
                     name="email"
                     type="email"
                     placeholder="Email"
                     required
-                    errors={[]}
+                    errors={state?.error?.fieldErrors.email}
                 />
-                <FormInput
+                <Input
                     name="username"
                     type="text"
                     placeholder="Username"
                     required
-                    errors={[]}
+                    errors={state?.error?.fieldErrors.username}
                 />
-                <FormInput
+                <Input
                     name="password"
                     type="password"
                     placeholder="Password"
                     required
-                    errors={state?.errors ?? []}
+                    errors={state?.error?.fieldErrors.password}
                 />
-                <FormButton text="Login" />
+                <Button text="Login" />
                 {state?.success ? (
                     <div className="flex justify-center font-semibold w-full m-auto text-white bg-green-500 p-2 rounded-xl">
-                        {state?.success}
+                        Welcome!
                     </div>
                 ) : (
                     ""
